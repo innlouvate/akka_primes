ccd785c50f32cd798c8918089d8486ddbb421f1e
innlouvate <l.j.franklin85@googlemail.com>
2017-08-02 22:51:54 +0100

    prime calc class

---
+++ src/application/PrimeCalculator.java
@@ -0,0 +1,21 @@
+package application;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+public class PrimeCalculator {
+
+    public static void main(String[] args) {
+        PrimeCalculator primeCalculator = new PrimeCalculator();
+        primeCalculator.calculate(1, 1000000);
+    }
+
+    public void calculate(int start, int end) {
+        int range[] = {start, end};
+        ActorSystem actorSystem = ActorSystem.create("PrimeCalculator");
+        ActorRef primeMaster = actorSystem.actorOf(Props.create(PrimeMaster.class, 10));
+        primeMaster.tell(range, primeMaster);
+    }
+
+}
